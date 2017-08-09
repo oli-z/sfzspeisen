c3b6b5c8bd38edede3cae6a0cdca622e2881db95
Oli Z <oli-z@users.noreply.github.com>
2017-08-09 10:10:04 +0200

    Anpassungen an neuen Plan

--- sfz.php
+++ sfz.php
@@ -1,16 +1,23 @@
 <?php
+// KEINE FEHLER ANZEIGEN //
+error_reporting(0);
+@ini_set('display_errors', 0);
 $spnr=$_GET['nr'];
 
 function getspeise($nr){
    $wday=date(N);
-   //$wday=1;
+   //$wday=7;
    $page = file_get_contents('http://www.sfz.rcs.de/');
-   $regexstr = '/(?s)(?<=menue-Menue'.$nr.' weekday-'.$wday.'">)(.+?)(?=<div class="menueIconContain">)/';
+   $regexstr = '/(?s)(?<=menue-'.$nr.' weekday-'.$wday.'">)(.+?)(?=<div class="menueIconContain">)/';
    preg_match (($regexstr),$page,$treffer);
    $speise=str_replace("<br />", " ", $treffer['0']);
    $speise=str_replace("- ","-",$speise);
    $speise=str_replace("- ","-",$speise);
-   $speise = substr($speise, 0, strpos($speise, "/"));
+   //$speise = substr($speise, 0, strpos($speise, "/"));
+   $speise = array_shift(explode("/", $speise));
+   //$speise = array_shift(explode("(", $speise));
+
+  
    return $speise;
 
 }
