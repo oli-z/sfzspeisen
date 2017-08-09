b107926fded8b25a1b8ba317a6d8a457f87d93c7
Oli Z <oli-z@users.noreply.github.com>
2017-08-09 17:10:21 +0200

    EMOJIS!

--- sfz.php
+++ sfz.php
@@ -1,25 +1,48 @@
 <?php
 // KEINE FEHLER ANZEIGEN //
-error_reporting(0);
-@ini_set('display_errors', 0);
+//error_reporting(0);
+//@ini_set('display_errors', 0);
 $spnr=$_GET['nr'];
+
+function cleanupstring($string){
+   $string = str_replace("<br />", " ", $string);
+   $string = str_replace("- ","-",$string);
+   $string = str_replace("- ","-",$string);
+  
+   $string = str_replace("(M1) ","",$string);
+   $string = str_replace("(M2) ","",$string);
+   $string = str_replace("(M3) ","",$string);
+   $string = str_replace("(Salat) ","",$string);
+   $string = str_replace("(L1) ","",$string);
+   $string = str_replace("(L2) ","",$string);
+  
+   $string = str_replace("&amp;","&",$string); // passt so
+   //$string = substr($speise, 0, strpos($string, "/"));
+   $string = array_shift(explode("/", $string));
+   //$string = array_shift(explode("(", $string));
+   return($string);
+}
+
+function emojify($string){
+  $string = str_replace('Ei', '🥚', $string);
+  $string = str_replace('Käse', '🧀', $string);
+  $string = str_replace('Kuchen', '🍰', $string);
+  $string = str_replace('Pasta', '🍝', $string);
+  $string = str_replace('Salat', '🥗', $string);
+  $string = str_replace('Tomate', '🍅', $string);
+  $string = str_replace('Wasser', '🥤', $string);
+  return($string);
+}
 
 function getspeise($nr){
    $wday=date(N);
-   //$wday=7;
+   //$wday=7; // für Tests am Wochenende
    $page = file_get_contents('http://www.sfz.rcs.de/');
    $regexstr = '/(?s)(?<=menue-'.$nr.' weekday-'.$wday.'">)(.+?)(?=<div class="menueIconContain">)/';
    preg_match (($regexstr),$page,$treffer);
-   $speise=str_replace("<br />", " ", $treffer['0']);
-   $speise=str_replace("- ","-",$speise);
-   $speise=str_replace("- ","-",$speise);
-   //$speise = substr($speise, 0, strpos($speise, "/"));
-   $speise = array_shift(explode("/", $speise));
-   //$speise = array_shift(explode("(", $speise));
-
-  
+   $speise=$treffer['0'];
+   $speise=cleanupstring($speise);
    return $speise;
-
 }
 
 
