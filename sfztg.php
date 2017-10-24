16769032449493d22130eac7a3d7c9dfe021520e
Oli Z <oli-z@users.noreply.github.com>
2017-10-24 11:29:56 +0200

    Update sfztg.php

--- sfztg.php
+++ sfztg.php
@@ -2,37 +2,39 @@
 <?php
 
 
-$tgkey = ''; //Telegram API Key
+$tgkey = 'DONTSTEALMYAPIKEY'; //Telegram API Key
 $group = '@sfzspeisen'; //Telegram Group
 
 require('sfz.php');
 error_reporting(E_ALL);
 @ini_set('display_errors', E_ALL);
+setlocale(LC_TIME, "de_DE.utf8");
+
+if(getspeise('Menue_1') == "" || getspeise('Menue_1') == " " || getspeise('Menue_1') == "  "){            // Programmierskills over 9000
+  die();
+}
+
 $datum=date('j.n.y');
 $text=
-  "Der Sepiseplan vom ".$datum.":".chr(10).chr(10).
+  "Der Speiseplan vom ".$datum." (".strftime("%A")."):".chr(10).chr(10).
   "🍲 1️⃣: ".emojify(getspeise('Menue_1')).chr(10).chr(10).
   "🍲 2️⃣: ".emojify(getspeise('Menue_2')).chr(10).chr(10).
   "🍲 3️⃣: ".emojify(getspeise('Menue_3')).chr(10).chr(10).
   "🥗: ".emojify(getspeise('Salat')).chr(10).chr(10).
-  "🍱 herzhaft: ".emojify(getspeise('Lunchbox_herzhaft')).chr(10).chr(10).
+  "🍱 herzhaft: " .emojify(getspeise('Lunchbox_herzhaft')).chr(10).chr(10).
   "🍱 süss: ".emojify(getspeise('Lunchbox_suess')).chr(10).chr(10).
-  "Guten Appetit!	\\xF0\\x9F\\x98\\x89";
-  //"Verpflegungsbeutel S: ".getspeise('Verpflegungsbeutel_S')
+  "🍿 S: ".getspeise('Verpflegungsbeutel_S').chr(10).chr(10).
+  "Guten Appetit!	🍽";
   
 
 print $text;
 print('<br>');
-//$text='Test, bitte ignorieren';
-//$temp=file_get_contents(('https://api.telegram.org/bot'.$tgkey.'/sendMessage?chat_id='.$group.'&text='.$text));
-//print $temp;
 
 $url = 'https://api.telegram.org/bot'.$tgkey.'/sendMessage';
 $data = array('chat_id' => $group,
               'text' => $text,
-              'disable_notification' => 'true');
+              'disable_notification' => 'false');
 
-// use key 'http' even if you send the request to https://...
 $options = array(
     'http' => array(
         'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
@@ -41,9 +43,7 @@ $options = array(
     )
 );
 $context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
+$result = file_get_contents($url, false, $context); // Das hier auskommentieren, um Senden zu verhindern (für Tests)
 var_dump($result);
-
-print('test');
 ?>
 </pre>
